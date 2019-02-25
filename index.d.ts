2af5b6c060eb491f7eeedd13cbb6802044c6f0c8
jonathan-saunders-ge <17344158+jonathan-saunders-ge@users.noreply.github.com>
2019-02-25 17:26:00 -0500

    add 'options' param to declaration in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 type Variant = 'RFC3548' | 'RFC4648' | 'RFC4648-HEX' | 'Crockford'
-declare function base32Encode(buffer: ArrayBuffer, variant: Variant): string
+declare function base32Encode(buffer: ArrayBuffer, variant: Variant, options: any): string
 export = base32Encode
